d09ebd903951382800531152a53536761257bd6d
Weilon1 <weilon.barbosa@gmail.com>
2024-08-18 12:36:14 -0300

    alterações no settings.py config STATIC_ROOT

--- app/settings.py
+++ app/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from datetime import timedelta
 
@@ -112,6 +113,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
 # Default primary key field type
